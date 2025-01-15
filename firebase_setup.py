613b665b36cf20183ba9e8e3ce55890209403312
Starlight <komangananda987@gmail.com>
2025-01-16 02:05:05 +0800

    Update firebase_setup.py

--- firebase_setup.py
+++ firebase_setup.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 # Menentukan path relatif ke file kredensial
-cred = credentials.Certificate('./firebase-credentials.json')  # Pastikan path relatif sesuai dengan lokasi file kredensial
+cred = credentials.Certificate('C:/Users/I Komang Gede/Desktop/Mangde/stock_dashboard/firebase-credentials.json')  # Pastikan path relatif sesuai dengan lokasi file kredensial
 
 # Menginisialisasi Firebase
 firebase_admin.initialize_app(cred)
